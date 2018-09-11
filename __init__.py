c8aeb917d2abc4b79086fb49df9b5725de253824
yeqing <heopro@163.com>
2018-09-11 22:15:44 +0800

    some change 2

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from .helper import (
     MouTemplate,
     make_response,
 )
-from .base_model import Model
+from .base_model import BaseModel
 from .session import Session
 from .utils import log
 from .run import run, request
